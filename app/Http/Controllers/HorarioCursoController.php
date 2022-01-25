032e307aeb924e14eb530f1e5e642763e40f0d51
eivier <eiviertest@gmail.com>
2022-01-25 17:18:54 -0600

    Creacion modelo, controlador, migracion de Horario de Curso

---
+++ app/Http/Controllers/HorarioCursoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\HorarioCurso;
+use Illuminate\Http\Request;
+
+class HorarioCursoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\HorarioCurso  $horarioCurso
+     * @return \Illuminate\Http\Response
+     */
+    public function show(HorarioCurso $horarioCurso)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\HorarioCurso  $horarioCurso
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(HorarioCurso $horarioCurso)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\HorarioCurso  $horarioCurso
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, HorarioCurso $horarioCurso)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\HorarioCurso  $horarioCurso
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(HorarioCurso $horarioCurso)
+    {
+        //
+    }
+}
