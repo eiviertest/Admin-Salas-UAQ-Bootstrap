fe5a59fc3af67739f5eb558820a9a45027eb3c00
Alan Herrera <alanherrera.2828@gmail.com>
2022-02-18 15:30:25 -0600

    Corrección de Renderizado en login y Registro, y en renderizado del home

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('content')
+@section('contenido')
 <div class="container">
     <div class="row justify-content-center d-flex align-items-center">
         <div class="col-md-6">
--- routes/web.php
+++ routes/web.php
@@ -27,10 +27,7 @@ Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('login', [LoginController::class, 'login']);
 //Auth::routes();
 
-//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/home', function () {
-    return view('contenido');
-})->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::group(['middleware' => ['auth']], function () {
     //Ruta para cerrar sesión
