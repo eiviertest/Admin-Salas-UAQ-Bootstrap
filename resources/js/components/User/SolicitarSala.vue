3a54d8de4f77fbdc8b5045f5503bd02eb2a876a4
Alan Herrera <alanherrera.2828@gmail.com>
2022-03-14 14:45:03 -0600

    Solicitud Sala Usuario - Acomodo de Formulario

--- resources/js/components/User/SolicitarSala.vue
+++ resources/js/components/User/SolicitarSala.vue
@@ -8,80 +8,6 @@
                     <div class="card-body">
                         <div class="row">
                             <form method="post" @submit.prevent="registrarSolicitud()" enctype="multipart/form-data">
-                                <div class="form-group row">
-                                    <div class="col-md-4">
-                                        <label class="form-control-label" for="text-input">Sala A Solicitar: *</label>
-                                        <select @change="getEventos()" required class="form-select" v-model="solicitud.idSala">
-                                            <option value="0" selected disabled>Seleccione una sala...</option>
-                                            <option :value="sala.idSala" v-text="sala.nomSala" v-for="sala in salas" :key="sala.idSala"></option>
-                                        </select>
-                                        <div v-if="errorSelect" class="alert alert-warning d-flex align-items-center" role="alert">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
-                                                <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-                                            </svg>
-                                            <div>
-                                                Seleccione una Sala por favor.
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-4">
-                                        <label class="form-control-label" for="text-input">Fecha: *</label>
-                                        <input @change="getEventos()" required type="date" v-model="solicitud.fecha" class="form-control">
-                                        <span class="is-invalid" v-if="errores && errores['solicitud.fecha']">
-                                            <strong>{{ errores['solicitud.fecha'][0] }}</strong>
-                                        </span>                                
-                                    </div>
-                                </div>
-                                <br>
-                                <div class="form-group row">
-                                    <div class="col-md-4">
-                                        <label class="form-control-label" for="text-input">Hora de inicio: *</label>
-                                        <input @change="getEventos()" required type="time" v-model="solicitud.horaIni" class="form-control" step="3600" min="08:00" max="17:00">
-                                        <span class="is-invalid" v-if="errores && errores['solicitud.horaIni']">
-                                            <strong>{{ errores['solicitud.horaIni'][0] }}</strong>
-                                        </span> 
-                                    </div>
-                                    <div class="col-md-4">
-                                        <label class="form-control-label" for="text-input">Hora de Fin: *</label>
-                                        <input @change="getEventos()" required type="time" v-model="solicitud.horaFin" class="form-control" step="3600" :min="solicitud.horaIni" max="18:00">
-                                        <span class="is-invalid" v-if="errores && errores['solicitud.horaFin']">
-                                            <strong>{{ errores['solicitud.horaFin'][0] }}</strong>
-                                        </span>
-                                    </div>
-                                </div>
-                                <br>
-                                <div class="row form-group">
-                                    <div class="col-md-4">
-                                        <label class="form-control-label">Formato de Solicitud: * <font-awesome-icon icon="fa-solid fa-file-invoice" /></label>
-                                        <input required accept="application/pdf" class="form-control" type="file" id="inputFormSol" @change="seleccionarArchivo">
-                                        <br>
-                                        <div v-if="errorFile" class="alert alert-warning d-flex align-items-center" role="alert">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
-                                                <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-                                            </svg>
-                                            <div>
-                                                El tipo de archivo es incorrecto, seleccione un <strong>PDF</strong> por favor
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="row form-group">
-                                    <div class="col-md-3">
-                                        <button type="submit" class="btn btn-primary">Solicitar Sala <font-awesome-icon icon="fa-solid fa-house-laptop" /></button>
-                                    </div>
-                                </div> <br>
-                            </form>
-                        </div>
-                        <div class="row">
-                            <label class="form-control-label h2">Eventos</label>
-                            <FullCalendar :options="calendarOptions"/>
-                        </div>
-                        <div class="vld-parent">
-                        <loading :active.sync="isLoading"
-                                :can-cancel="false"
-                                :is-full-page="true"/>
-                        </div>
-                        <form method="post" @submit.prevent="registrarSolicitud()" enctype="multipart/form-data">
                             <div class="form-group row">
                                 <div class="col-md-4">
                                     <label class="form-control-label" for="text-input">Sala A Solicitar: *</label>
@@ -159,6 +85,16 @@
                                 </div>
                             </div>
                         </form>
+                        </div>
+                        <div class="row">
+                            <label class="form-control-label h2">Eventos</label>
+                            <FullCalendar :options="calendarOptions"/>
+                        </div>
+                        <div class="vld-parent">
+                        <loading :active.sync="isLoading"
+                                :can-cancel="false"
+                                :is-full-page="true"/>
+                        </div>
                     </div>
                 </div>
             </div>
